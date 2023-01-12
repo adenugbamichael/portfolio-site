59676c48aa65c9d72b054f53e35652dae76021fb
adenugbamichael <adenugbamicke@gmail.com>
2023-01-12 11:48:16 +0100

    Seperate Page Layouts into header, navbar, blog, portfolio and footer components

---
+++ components/blogs/blogList/BlogItem.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const BlogItem = ({ blog }: any) => {
+  return (
+    <div className='group'>
+      <div className='h-80 aspect-w-1 aspect-h-1 w-full rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40'>
+        <Link legacyBehavior href={`/blogs/${blog.slug}`}>
+          <div className='relative h-80 aspect-w-1 aspect-h-1 w-full rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40'>
+            <Image
+              priority
+              layout='fill'
+              objectFit='cover'
+              src={blog.coverImage}
+              className='rounded-lg hover:cursor-pointer'
+              alt={""}
+            />
+          </div>
+        </Link>
+      </div>
+      <div className='mt-4 flex justify-between'>
+        <div>
+          <h3 className='text-sm text-gray-700 font-bold'>
+            <span aria-hidden='true' className='inset-0' />
+            {blog.title}
+          </h3>
+          <p className='mt-1 text-sm text-gray-500'>{blog.description}</p>
+        </div>
+      </div>
+      <Link legacyBehavior href={`/blogs/${blog.slug}`}>
+        <a className='text-sm font-bold text-gray-700'>Read More</a>
+      </Link>
+    </div>
+  )
+}
